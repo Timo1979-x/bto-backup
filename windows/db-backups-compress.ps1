57736ecc13bd536ec54be737a8e3b3395e554b10
Tim Lukashevich <silvernet@yandex.ru>
2025-04-24 13:34:44 +0300

    uncompressedFolder for compressing script

--- windows/db-backups-compress.ps1
+++ windows/db-backups-compress.ps1
@@ -1,12 +1,13 @@
 # file must be saved in UTF16 LE
-# runas /savecred /user:maingto\schedulingUser "powershell d:\temp\backup\windows\compress-backups.ps1"
+# runas /savecred /user:maingto\schedulingUser "powershell d:\bto-backup\windows\db-backups-compress.ps1"
 
 invoke-expression -Command $PSScriptRoot\..\vars\mssql-db.ps1
 invoke-expression -Command $PSScriptRoot\..\messaging\messaging.ps1
 invoke-expression -Command $PSScriptRoot\funcs.ps1
 
 $backupFolder = $Global:dbDefaultBackupFolder
-$uncompressedFolder = $Global:dbUncompressedFolder
+# $uncompressedFolder = "$Global:dbUncompressedFolder"
+$uncompressedFolder = "d:\backups\_uncompressed"
 $backups = Get-ChildItem -Path "$uncompressedFolder\*.bak" -Recurse
 
 foreach ($backup in $backups) {
@@ -18,7 +19,7 @@ foreach ($backup in $backups) {
   Send-Telegram("${fullName}: compress started")
   $t1 = Get-Date
   New-Item -Path "$backupFolder\$dbName" -ItemType Directory
-  d:\backup\scripts\rar.exe a -ep -ag-YY-MM-DD_HH-MM-SS -df -m3 -ma5 -md256m -mt24 -t -rr -s "$backupFolder\$dbName\$fileName.rar" "$fullName"
+  & "$PSScriptRoot\rar.exe" a -ep -ag-YY-MM-DD_HH-MM-SS -df -m3 -ma5 -md256m -mt24 -t -rr -s "$backupFolder\$dbName\$fileName.rar" "$fullName"
   $t2 = Get-Date
   Send-Telegram("${fullName}: compress finished in time " + ($t2 - $t1).ToString())
 }
